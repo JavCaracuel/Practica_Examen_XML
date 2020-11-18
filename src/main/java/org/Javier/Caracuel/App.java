62caedeef21642fa88d60d625a2d4834374fa172
Javier <javcaracuel1@gmail.com>
2020-11-18 11:42:52 +0100

    Seguimos tarbajando en XML Parse

--- src/main/java/org/Javier/Caracuel/App.java
+++ src/main/java/org/Javier/Caracuel/App.java
@@ -1,12 +1,17 @@
 package org.Javier.Caracuel;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.File;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
 
@@ -66,6 +71,47 @@ public class App
 
 
             }while (menu!=2);
+
+
+
+    }
+
+    public static void Leer_Fichero(){
+
+        File file = new File("Concesionario.xml");
+
+        try {
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(file);
+            doc.getDocumentElement().normalize();
+            NodeList nList = doc.getElementsByTagName("coche");
+
+            for(int temp = 0; temp < nList.getLength(); temp++) {
+                Node nNode = nList.item(temp);
+
+                if(nNode.getNodeType() == Node.ELEMENT_NODE) {
+                    Element eElement = (Element) nNode;
+
+                    System.out.println("\nCoche id: " + eElement.getAttribute("id"));
+                    System.out.println("Marca: "
+                            + eElement.getElementsByTagName("marca").item(0).getTextContent());
+                    System.out.println("Modelo: "
+                            + eElement.getElementsByTagName("modelo").item(0).getTextContent());
+                    System.out.println("Cilindrada: "
+                            + eElement.getElementsByTagName("cilindrada").item(0).getTextContent());
+                }
+            }
+
+
+
+
+
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+
+
 
 
 
