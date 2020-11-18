68516486d7edca2267b0f67b3fb04c30c42389e0
Javier <javcaracuel1@gmail.com>
2020-11-18 10:37:55 +0100

    Primer commit

---
+++ src/main/java/org/Javier/Caracuel/App.java
@@ -0,0 +1,13 @@
+package org.Javier.Caracuel;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
