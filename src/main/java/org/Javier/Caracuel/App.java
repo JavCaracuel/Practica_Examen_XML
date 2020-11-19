eef7b4c11f59b6a35fde665f7818f8bc3ce02ef9
Javier <javcaracuel1@gmail.com>
2020-11-19 12:49:16 +0100

    Anadimos librerias necesarias

--- src/main/java/org/Javier/Caracuel/App.java
+++ src/main/java/org/Javier/Caracuel/App.java
@@ -1,13 +1,14 @@
 package org.Javier.Caracuel;
 
 import com.sun.org.apache.xpath.internal.operations.Mod;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
+import org.w3c.dom.*;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.InputMismatchException;
@@ -156,6 +157,6 @@ public class App
 
 
     }
-
+  
 
 }
